175cd473519a53b8059d6dfb2d866a1f3be4c6b3
Muntasir-Mahmud <m.siyam529@gmail.com>
2023-09-07 20:54:23 +0600

    Fixed some bugs and typo

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from langchain.chains import ConversationalRetrievalChain
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.llms import HuggingFaceHub
 from langchain.memory import ConversationBufferMemory
@@ -11,8 +12,9 @@ def get_pdf_text(pdf_docs):
     text = ''
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
+        for i in range(22,22):
+            text += pdf_reader.pages[i].extract_text()
+    print("PDF extraction completed")
     return text
 
 def get_text_chunks(text):
@@ -22,19 +24,21 @@ def get_text_chunks(text):
         chunk_overlap=200,
         length_function=len)
     chunks = text_splitter.split_text(text)
+    print("Convert to text chunk is completed")
     return chunks
 
 
 def get_vectorstore(text_chunks):
     embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    print("Vector store is created")
     return vectorstore
 
 def get_conversation_chain(vectorstore):
     llm = HuggingFaceHub(
         repo_id="google/flan-t5-xxl",
         model_kwargs={"temperature":0.5, "max_length":512})
-    memory_key = ConversationBufferMemory(
+    memory = ConversationBufferMemory(
         memory_key='chat_history',
         return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
